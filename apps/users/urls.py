51856b9cb06c3919b997c6c7e7d4c8e0c72b3c48
Jorge V. M. M <joevzq@gmail.com>
2020-04-17 19:32:19 -0500

    Use django urls for login and password reset

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('password-reset/', 
         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), 
-        name='reset_password'),
+        name='password_reset'),
     path('password-reset-sent/', 
         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_sent.html'), 
         name='password_reset_done'),
