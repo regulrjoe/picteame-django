ac7a15b6663ab987773a1b4833b10ab1109113e5
regulrjoe <jrgvmm@gmail.com>
2024-01-18 01:05:24 +0000

    Install dependencies and add Pipfiles

--- picteame/settings.py
+++ picteame/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'picteame.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'APP',
+        'NAME': 'picteame',
         'USER': 'joevzq',
         'PASSWORD': 942902,
         'HOST': 'localhost',
