36fa446f8cce47e2de2a787f1329ca39f7ba2e16
regulrjoe <jrgvmm@gmail.com>
2024-01-25 01:49:37 +0000

    get rid of is_admin. Add Admin Panel to navbar.

--- apps/users/models.py
+++ apps/users/models.py
@@ -31,7 +31,6 @@ class TalentAccount(AbstractBaseUser):
 
     created_at     = models.DateTimeField(verbose_name='created_at', auto_now_add=True)
     last_login      = models.DateTimeField(verbose_name='last login', auto_now=True)
-    is_admin        = models.BooleanField(default=False)
     is_active       = models.BooleanField(default=True)
     is_staff        = models.BooleanField(default=False)
     is_superuser    = models.BooleanField(default=False)
@@ -45,7 +44,7 @@ class TalentAccount(AbstractBaseUser):
         return self.email
 
     def has_perm(self, perm, obj=None):
-        return self.is_admin
+        return self.is_staff
 
     def has_module_perms(self, app_label):
         return True
